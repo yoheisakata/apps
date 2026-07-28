144659903af2ac18b25e9d1bccf1a4ba8b9e72eb
Yohei Sakata <yoheisakata80@gmail.com>
2026-07-28 01:53:21 -0700

    boardgames: アプリアイコンをサイコロ(5の目)デザインに変更

--- boardgames/make-icon.swift
+++ boardgames/make-icon.swift
@@ -1,5 +1,5 @@
 #!/usr/bin/env swift
-// 薄い青グラデーション背景に、将棋駒・チェスナイト・碁石(黒白)を描いたアプリアイコンを生成する。
+// 薄い青グラデーション背景に、白いサイコロ(5の目)を描いたアプリアイコンを生成する。
 //   実行: swift make-icon.swift   →  Resources/AppIcon.png (2048x2048) を出力
 // build.sh の build_icns() がこのマスター PNG から iconset/icns を都度生成するので、
 // ここでは iconset/icns は作らない。
@@ -8,11 +8,9 @@ import AppKit
 let blueTop = NSColor(srgbRed: 0.56, green: 0.78, blue: 0.95, alpha: 1)    // #8FC7F2
 let blueBottom = NSColor(srgbRed: 0.31, green: 0.56, blue: 0.78, alpha: 1) // #4F8FC7
 
-let cream = NSColor(srgbRed: 0.976, green: 0.941, blue: 0.847, alpha: 1) // #F9F0D8
-let brown = NSColor(srgbRed: 0.42, green: 0.26, blue: 0.15, alpha: 1)    // 駒の縁
 let white = NSColor.white
-let black = NSColor.black
-let stoneStroke = NSColor(srgbRed: 0.22, green: 0.40, blue: 0.56, alpha: 1)
+let pipBlue = NSColor(srgbRed: 0.31, green: 0.56, blue: 0.78, alpha: 1)   // 目の色(背景の青と揃える)
+let dieShadow = NSColor(white: 0, alpha: 0.18)
 
 func renderIcon(pixels: Int) -> NSBitmapImageRep {
     let rep = NSBitmapImageRep(
@@ -35,72 +33,39 @@ func renderIcon(pixels: Int) -> NSBitmapImageRep {
     let gradient = NSGradient(colors: [blueTop, blueBottom])!
     gradient.draw(in: bgPath, angle: -90)
 
-    // 将棋駒(左上): 五角形(先細り、末広がり)
-    let cx1 = s * 0.283
-    let yTop = s * 0.84
-    let yShoulder = s * 0.74
-    let yBottom = s * 0.50
-    let halfBottomW = s * 0.142
-    let halfShoulderW = s * 0.105
-
-    let koma = NSBezierPath()
-    koma.move(to: NSPoint(x: cx1, y: yTop))
-    koma.line(to: NSPoint(x: cx1 + halfShoulderW, y: yShoulder))
-    koma.line(to: NSPoint(x: cx1 + halfBottomW, y: yBottom))
-    koma.line(to: NSPoint(x: cx1 - halfBottomW, y: yBottom))
-    koma.line(to: NSPoint(x: cx1 - halfShoulderW, y: yShoulder))
-    koma.close()
-    cream.setFill()
-    koma.fill()
-    brown.setStroke()
-    koma.lineWidth = s * 0.012
-    koma.stroke()
-
-    let kanji = "王"
-    let kanjiFont = NSFont(name: "HiraginoSans-W6", size: s * 0.2) ?? NSFont.boldSystemFont(ofSize: s * 0.2)
-    let kanjiAttrs: [NSAttributedString.Key: Any] = [.font: kanjiFont, .foregroundColor: black]
-    let kanjiStr = NSAttributedString(string: kanji, attributes: kanjiAttrs)
-    let kanjiSize = kanjiStr.size()
-    kanjiStr.draw(at: NSPoint(x: cx1 - kanjiSize.width / 2, y: (yBottom + yShoulder) / 2 - kanjiSize.height / 2))
-
-    // チェスナイト(右上)
-    let cx2 = s * 0.645
-    let cy2 = s * 0.675
-    let knightText = "♞"
-    let knightFont = NSFont.systemFont(ofSize: s * 0.5)
-    let knightAttrs: [NSAttributedString.Key: Any] = [.font: knightFont]
-    let knightStr = NSAttributedString(string: knightText, attributes: knightAttrs)
-    let knightSize = knightStr.size()
-    let knightOrigin = NSPoint(x: cx2 - knightSize.width / 2, y: cy2 - knightSize.height / 2)
+    // サイコロ(白、5の目)を中央に正面向きで配置
+    let dieSide = s * 0.56
+    let dieRadius = dieSide * 0.18
+    let dieRect = NSRect(x: -dieSide / 2, y: -dieSide / 2, width: dieSide, height: dieSide)
+    let diePath = NSBezierPath(roundedRect: dieRect, xRadius: dieRadius, yRadius: dieRadius)
 
     g.saveGState()
-    g.beginTransparencyLayer(auxiliaryInfo: nil)
-    knightStr.draw(at: knightOrigin)
-    g.setBlendMode(.sourceIn)
-    g.setFillColor(white.cgColor)
-    g.fill(CGRect(x: 0, y: 0, width: s, height: s))
-    g.endTransparencyLayer()
+    g.translateBy(x: s / 2, y: s / 2)
+
+    // 落ち影
+    g.saveGState()
+    g.translateBy(x: s * 0.012, y: -s * 0.012)
+    dieShadow.setFill()
+    diePath.fill()
     g.restoreGState()
 
-    // 碁石(下段): 黒石・白石
-    let stoneR = s * 0.13
-    let stoneY = s * 0.254
-    let blackCx = s * 0.341
-    let whiteCx = s * 0.634
-
-    let blackStone = NSBezierPath(ovalIn: NSRect(x: blackCx - stoneR, y: stoneY - stoneR, width: stoneR * 2, height: stoneR * 2))
-    black.setFill()
-    blackStone.fill()
-    stoneStroke.setStroke()
-    blackStone.lineWidth = s * 0.004
-    blackStone.stroke()
-
-    let whiteStone = NSBezierPath(ovalIn: NSRect(x: whiteCx - stoneR, y: stoneY - stoneR, width: stoneR * 2, height: stoneR * 2))
     white.setFill()
-    whiteStone.fill()
-    stoneStroke.setStroke()
-    whiteStone.lineWidth = s * 0.004
-    whiteStone.stroke()
+    diePath.fill()
+
+    // 目(5の目): 四隅+中央
+    let pipR = dieSide * 0.09
+    let pipOffset = dieSide * 0.245
+    let pipCenters = [
+        NSPoint(x: -pipOffset, y: pipOffset), NSPoint(x: pipOffset, y: pipOffset),
+        NSPoint(x: 0, y: 0),
+        NSPoint(x: -pipOffset, y: -pipOffset), NSPoint(x: pipOffset, y: -pipOffset),
+    ]
+    pipBlue.setFill()
+    for c in pipCenters {
+        let pip = NSBezierPath(ovalIn: NSRect(x: c.x - pipR, y: c.y - pipR, width: pipR * 2, height: pipR * 2))
+        pip.fill()
+    }
+    g.restoreGState()
 
     NSGraphicsContext.restoreGraphicsState()
     return rep
